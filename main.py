1235d6776dd471438d384a2a06fea1a8efbc6bc2
mzrithm <74382959+mzrithm@users.noreply.github.com>
2021-08-11 21:29:09 -0700

    Created Paddle class and Paddle move_up, move_down methods.

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+# screen.tracer(0)  # Turns off screen animation
 
 paddle = Paddle()
-screen.onkeypress(paddle.move_up, "Up")
-screen.onkeypress(paddle.move_down, "Down")
+screen.onkey(paddle.move_up, "Up")
+screen.onkey(paddle.move_down, "Down")
 screen.listen()
+
+game_is_on = True
+
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -8,36 +8,23 @@ class Paddle:
         """Initializes paddle object with width, height,
         and starting coordinates
         """
-        self.x_pos = 350
-        self.y_pos = 0
         self.paddle = Turtle()
+        self.x_pos = self.paddle.xcor()
+        self.y_pos = self.paddle.ycor()
         self.paddle.hideturtle()
         self.paddle.resizemode("user")
         self.paddle.turtlesize(stretch_wid=5, stretch_len=1)
         self.paddle.penup()
         self.paddle.shape("square")
         self.paddle.color("white")
-        self.paddle.setpos(self.x_pos, self.y_pos)
+        self.paddle.setpos(350, 0)
         self.paddle.showturtle()
 
-    # def create_paddle(self):
-    #     paddle = Turtle()
-    #     paddle.hideturtle()
-    #     paddle.resizemode("user")
-    #     paddle.turtlesize(stretch_wid=5, stretch_len=1)
-    #     paddle.penup()
-    #     paddle.shape("square")
-    #     paddle.color("white")
-    #     paddle.setpos(350, 0)
-    #     paddle.showturtle()
-
     def move_up(self):
-        self.y_pos += 20
-        self.paddle.goto(self.x_pos, self.y_pos)
+        self.paddle.goto(self.paddle.xcor(), self.paddle.ycor() + 20)
 
     def move_down(self):
-        self.y_pos -= 20
-        self.paddle.goto(self.x_pos, self.y_pos)
+        self.paddle.goto(self.paddle.xcor(), self.paddle.ycor() - 20)
 
 
 
