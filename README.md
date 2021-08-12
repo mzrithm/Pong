e52d5a480823fd116b73559b342227750af83336
mzrithm <74382959+mzrithm@users.noreply.github.com>
2021-08-11 19:27:49 -0700

    Created screen.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Pong
 Pong Game in Python
+
+**Create Screen**
+- Height of 600px, Width of 800px
+- Black background color
+- Exit on click
---
+++ main.py
@@ -0,0 +1,9 @@
+from turtle import Screen
+
+screen = Screen()
+screen.listen()
+
+screen.bgcolor("black")
+screen.screensize(800, 600)
+
+screen.exitonclick()
